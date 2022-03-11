d53799d376dd7036ba60eb609af307fee707b7ea
KajaDabrowska <dabrowska.kaja.z@gmail.com>
2022-03-11 15:33:50 +0100

    fixed small history preview mistake, changed onClick to be a switch and not a if else statement

--- src/components/calculator/calculator.component.jsx
+++ src/components/calculator/calculator.component.jsx
@@ -7,9 +7,6 @@ import Keypad from "../keypad/keypad.component";
 //TODO
 // make maximum width (lengts) of the calculation, otherwise the calculator will be wide af
 
-//TODO
-// if else block into switch case statements
-
 const Calculator = () => {
   const [result, setResult] = useState("");
   const [displayResult, setDisplayResult] = useState("");
@@ -19,6 +16,12 @@ const Calculator = () => {
   const toggleParenthesisIsOpen = () => setParenthesisOpen(!parenthesisIsOpen);
 
   //   FUNCS
+
+  // const setResultAndDisplay = (mathRes, mathDis) => {
+  //   setResult(mathRes);
+  //   setDisplayResult(mathDis);
+  // };
+
   const reset = () => {
     setResult("");
     setDisplayResult("");
@@ -30,13 +33,33 @@ const Calculator = () => {
     setResult(result.slice(0, -1));
   };
 
+  const parenthesisFunc = () => {
+    if (!parenthesisIsOpen) {
+      setDisplayResult(displayResult + "(");
+      setResult(result + "(");
+      toggleParenthesisIsOpen();
+    } else {
+      setDisplayResult(displayResult + ")");
+      setResult(result + ")");
+      toggleParenthesisIsOpen();
+    }
+  };
+
   const calculate = () => {
     try {
       //set prev value
       setPrevValue(displayResult);
 
+      //TODO (rounding up etc)
       // eslint-disable-next-line
       const value = (eval(result) || "0") + "";
+      console.log(parseInt(value).toString() + (value % 1));
+      console.log(parseInt(value + (value % 1)));
+      // console.log(value.slice(2, 4));
+
+      console.log(value.valueOf());
+      console.log(parseInt(value).toFixed(3));
+      // console.log(parseInt(value).toFixed(6));
 
       setResult(value);
       setDisplayResult(value);
@@ -48,34 +71,32 @@ const Calculator = () => {
   };
 
   const onClick = (button) => {
-    // IMPORTANT
-    // the whole bug was cause by not having else in front of if xD
+    //TODO what when someone clicks "=" when it's 0
+    switch (button) {
+      case "=":
+        calculate();
+        break;
+      case "C":
+        reset();
+        break;
+      case "CE":
+        backspace();
+        break;
+      case "()":
+        parenthesisFunc();
+        break;
+      case "/":
+        setDisplayResult(displayResult + "÷");
+        setResult(result + button);
+        break;
+      case "*":
+        setDisplayResult(displayResult + "×");
+        setResult(result + button);
+        break;
 
-    if (button === "=") {
-      calculate();
-    } else if (button === "C") {
-      reset();
-    } else if (button === "CE") {
-      backspace();
-    } else if (button === "()") {
-      if (!parenthesisIsOpen) {
-        setDisplayResult(displayResult + "(");
-        setResult(result + "(");
-        toggleParenthesisIsOpen();
-      } else {
-        setDisplayResult(displayResult + ")");
-        setResult(result + ")");
-        toggleParenthesisIsOpen();
-      }
-    } else if (button === "/") {
-      setDisplayResult(displayResult + "÷");
-      setResult(result + button);
-    } else if (button === "*") {
-      setDisplayResult(displayResult + "×");
-      setResult(result + button);
-    } else {
-      setDisplayResult(displayResult + button);
-      setResult(result + button);
+      default:
+        setDisplayResult(displayResult + button);
+        setResult(result + button);
     }
   };
 
@@ -92,3 +113,34 @@ const Calculator = () => {
 };
 
 export default Calculator;
+
+//onClick with else if statements:
+// IMPORTANT
+// the whole bug was cause by not having else in front of if xD
+
+// if (button === "=") {
+//   calculate();
+// } else if (button === "C") {
+//   reset();
+// } else if (button === "CE") {
+//   backspace();
+// } else if (button === "()") {
+//   if (!parenthesisIsOpen) {
+//     setDisplayResult(displayResult + "(");
+//     setResult(result + "(");
+//     toggleParenthesisIsOpen();
+//   } else {
+//     setDisplayResult(displayResult + ")");
+//     setResult(result + ")");
+//     toggleParenthesisIsOpen();
+//   }
+// } else if (button === "/") {
+//   setDisplayResult(displayResult + "÷");
+//   setResult(result + button);
+// } else if (button === "*") {
+//   setDisplayResult(displayResult + "×");
+//   setResult(result + button);
+// } else {
+//   setDisplayResult(displayResult + button);
+//   setResult(result + button);
+// }
--- src/components/result/result.component.jsx
+++ src/components/result/result.component.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Result = ({ result, prevValue }) => {
   return (
     <div className="calc__window">
-      <div className="window__previous">{prevValue}</div>
+      <div className="window__previous">
+        {prevValue && prevValue !== "0" ? prevValue : "previous"}
+      </div>
       <div className="window__current">{result ? result : "0"}</div>
     </div>
   );
